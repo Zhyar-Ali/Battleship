eaf9aa2e50b9424ee9bfbba3bda4775cc31593e2
Zhyar <zhiarali595@gmail.com>
2026-09-12 10:19:37 +0300

    add test for placeShip

--- script.test.js
+++ script.test.js
@@ -1,5 +1,35 @@
-import { calculate } from "./script.js";
+import { Gameboard } from "./script.js";
 
-test("addition is working", () => {
-    expect(calculate().add(1,2)).toBe(3);
+describe("placeship", () => {
+    test("place ship horizontally", () => {
+        const s = {length: 3};
+        const g = Gameboard();
+        g.placeShip(s,5,5,"horizontal");
+        expect(g.grid[4][4]).toBe(1);
+        expect(g.grid[4][5]).toBe(1);
+        expect(g.grid[4][6]).toBe(1);
+    });
+
+    test("place ship vertically", () => {
+        const s = {length: 3};
+        const g = Gameboard();
+        g.placeShip(s,5,5,"vertical");
+        expect(g.grid[4][4]).toBe(1);
+        expect(g.grid[3][4]).toBe(1);
+        expect(g.grid[2][4]).toBe(1);
+    });
+
+    test("ship not placed due to collision", () => {
+        const s1 = {length: 3};
+        const s2 = {length: 3};
+        const g = Gameboard();
+        g.placeShip(s1,5,5,"vertical");
+        g.placeShip(s2,7,5,"vertical");
+        expect(g.grid[4][4]).toBe(1);
+        expect(g.grid[3][4]).toBe(1);
+        expect(g.grid[2][4]).toBe(1);
+        expect(g.grid[6][4]).toBe(0);
+        expect(g.grid[5][4]).toBe(0);
+    });
+
 });
