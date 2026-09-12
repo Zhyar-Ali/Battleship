5db74e6010dc94e34defd3ff00e0c0aeb6820238
Zhyar <zhiarali595@gmail.com>
2026-09-12 13:27:36 +0300

    add receiveAttack function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const Ship = function(lengthShip) {
+export const Ship = function( lengthShip) {
     const length = lengthShip;
     let numHit = 0;
 
     const hit = () => {
         if(numHit<length) {
-            numHit++;
+            numHit += 1;
         }
     };
 
@@ -14,11 +14,17 @@ const Ship = function(lengthShip) {
         }
     };
 
-    return {hit, isSunk, length};
+    const getHit = () => {
+        return numHit;
+    };
+
+    return {hit, isSunk, getHit, numHit,length};
 };
 
 export const Gameboard = function() {
     const grid = Array(10).fill().map(() => Array(10).fill(0));
+    const arrShip = [];
+    let arrMissedCoor = [];
 
     const placeShip = (ship, x, y, orientation) => {
         let isValid = true;
@@ -31,9 +37,10 @@ export const Gameboard = function() {
                     }
                 });
                 if(isValid) {
-                    for(let i = y-1; i < y+ship.length; i++) {
-                        grid[x-1][i] = 1;
+                    for(let i = y-1; i < y+ship.length-1; i++) {
+                        grid[x-1][i] = ship;
                     }
+                    arrShip.push(ship);
                 }
             }
             if(orientation === "vertical" && x-ship.length >= 0) {
@@ -44,17 +51,37 @@ export const Gameboard = function() {
                 }); 
                 if(isValid) {
                     for(let i = x-1; i >= x-ship.length; i--) {
-                        grid[i][y-1] = 1;
+                        grid[i][y-1] = ship;
                     }
+                    arrShip.push(ship);
                 }
             }
         }
     }
-    return {placeShip, grid};
+
+    const receiveAttack = (x, y) => {
+        if(x>=1 && x<=10 && y>=1 && y<=10) {
+            const target = grid[x-1][y-1];
+            
+            if(target !== 0 && target !== "X") {
+                target.hit();
+                grid[x-1][y-1] = "X";
+            }
+            if(target === 0) {
+                grid[x-1][y-1] = "X"
+                arrMissedCoor.push([x,y]);
+            }else{
+                console.log("Coordinate already hit");
+            }
+        }
+    };
+    return {placeShip, receiveAttack, grid};
 };
-// let g = Gameboard();
-// let s = Ship(3);
+let g = Gameboard();
+let s = Ship(3);
 // let s2 = Ship(3);
-// g.placeShip(s,5,5,"horizontal");
-// g.placeShip(s2,4,5,"vertical");
-// g.grid.forEach(value => console.log(value));
+g.placeShip(s,5,5,"horizontal");
+g.receiveAttack(3,5);
+// g.placeShip(s2,4,3,"vertical");
+g.grid.forEach(value => console.log(value));
+//prints 0 when it should be x
